2084c99aac17870e76a1ea2b61fec84833d40e45
wcwutw <alexwcwu@gmail.com>
2025-03-19 00:21:16 +0800

    [P] modify generator

--- P/gen/gen.cpp
+++ P/gen/gen.cpp
@@ -3,11 +3,12 @@
 #include <string.h>
 #include "testlib.h"
 using namespace std;
-const int N=1000000;
+int N=1000000;
 
 int main(int argc, char* argv[]) {
 	registerGen(argc, argv, 1);
 	int rnd_seed=atoi(argv[1]);
-	int n=(atoi(argv[2])==0?rnd.next(N/10, N):n);
+	int n=atoi(argv[2]);
+	if(n==0) n=rnd.next(N/10, N);
 	cout<<n<<"\n";
 }
