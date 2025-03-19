79f1aa14094c0c89683c1ab5b2e015c38d620b9f
Ayden Chang <poshen.chang@gmail.com>
2025-03-20 00:46:43 +0800

    update gen

--- H/gen/gen.cpp
+++ H/gen/gen.cpp
@@ -2,9 +2,12 @@
 #include <cassert>
 
 int main(int argc, char* argv[]) {
-    assert(argc > 2);
+    assert(argc > 1);
     registerGen(argc, argv, 1);
-    int n = atoi(argv[1]);
-    int m = atoi(argv[2]);
-    printf("%d %d\n", n, m);
+    int rnd_seed = atoi(argv[1]);
+    int MAXN = 100000;
+    int n = rnd.next(MAXN) + 1;
+    int t = rnd.next(MAXN) + 1;
+    int k = rnd.next(n) + 1;
+    printf("%d %d %d\n", n, k, t);
 }
