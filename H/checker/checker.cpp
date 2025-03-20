cb12ae09254873ba4d274a66cdf44dda448cd961
Ayden Chang <poshen.chang@gmail.com>
2025-03-20 10:35:31 +0800

    update checker

--- H/checker/checker.cpp
+++ H/checker/checker.cpp
@@ -21,28 +21,12 @@ void check(bool res) {
 
 const int MAXC = 1000000000;
 std::set<int> vis;
-int N, M;
-
-int readAns(InStream &stream) {
-    int cnt = 0;
-    for (int i = 1; i <= M; i++) {
-        if (stream.seekEof()) break;
-        int A = stream.readInt(0, MAXC, "A_" + std::to_string(cnt));
-        int B = stream.readInt(0, MAXC, "B_" + std::to_string(cnt));
-        if (A + B == N && vis.find(A) == vis.end())
-            vis.insert(A), ++cnt;
-    }
-    return cnt;
-}
 
 int main(int argc, char *argv[]) {
     registerTestlibCmd(argc, argv);
-    N = inf.readInt();
-    M = inf.readInt();
-    int cnt = readAns(ouf);
-    if (cnt > M || cnt == 0)
-        exit_WA();
-    else if (cnt == M)
+    int N = ouf.readInt();
+    int M = ans.readInt();
+    if (N == M && ouf.seekEof() && ans.seekEof())
         exit_AC();
     else
         exit_WA();
