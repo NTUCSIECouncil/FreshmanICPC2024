6fd89f43e7da4f9fe0768029745680d5464fe496
PixelCat31415 <jimmychiu31415@gmail.com>
2025-03-23 11:36:52 +0800

    [statement] minor fix on containers
    - remove exited container properly
    - abort when container could not be started

--- statement/stategen.sh
+++ statement/stategen.sh
@@ -4,12 +4,17 @@ CONTAINER_NAME="freshmanicpc-stategen"
 
 script_path="$(realpath $0)"
 
+if [ "$(docker ps -aq -f status=exited -f name=$CONTAINER_NAME)" ]; then
+    docker rm $CONTAINER_NAME
+fi
 if [ ! "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=$CONTAINER_NAME)" ]; then
-        docker rm $CONTAINER_NAME
-    fi
     docker build "${script_path%/*}" -t $CONTAINER_NAME
     docker run -d --name $CONTAINER_NAME -v "$(realpath "${script_path%/*}/.."):/FreshmanICPC2024" $CONTAINER_NAME
+fi
+
+if [ ! "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; then
+    echo "Could not start docker container."
+    exit 1
 fi
 
 container_script="cd /FreshmanICPC2024; python statement/stategen.py $@"
