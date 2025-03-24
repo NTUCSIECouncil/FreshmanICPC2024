feca5e1ae167f53b1843183da87daac22337bfc2
JerryTsai <b11902040@csie.ntu.edu.tw>
2025-03-24 18:53:11 +0800

    [P] minor fix

--- J/solution/sol.cpp
+++ J/solution/sol.cpp
@@ -3,5 +3,5 @@ using namespace std;
 
 int main() {
     long long a, b;
-    cout << "CSIE{My_n4m3_15_B45737}";
+    cout << "CSIE{My_n4m3_15_B45737}" << endl;
 }
--- J/validator/val.cpp
+++ J/validator/val.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main(int argc, char **argv) {
     registerValidation(argc, argv);
-    int a = inf.readInt(0, 2000000000);
-    inf.readSpace();
-    int b = inf.readInt(0, 2000000000);
-    inf.readEoln();
+    // int a = inf.readInt(0, 2000000000);
+    // inf.readSpace();
+    // int b = inf.readInt(0, 2000000000);
+    // inf.readEoln();
     inf.readEof();
 }
